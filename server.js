d90da455e52c8fdd90bbad91fe5c6fefadbefb5c
Rizki Andrianto <rizki_andrianto@rocketmail.com>
2016-11-13 23:43:47 +0700

    add header

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.listen(port, function() {
   console.log('listening on ' + port);
 })
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", 'GET, POST, PUT ,DELETE');
+    next();
+});
+
 app.get('/', function (req, res) {
   res.send('Welcome to Rizki\'s Blog')
 })
